e2f509e38e512f6051ea292d4488c8f35b9d40c7
Mohamed Aymn <mohamedaymn218@gmail.com>
2025-09-04 23:43:40 +0300

    feat: contact us form

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -1,15 +1,51 @@
-import HeroSection from '@/components/shared/sections/heroSection'
+import FormAbstraction, { FieldMeta } from '@/components/shared/form/formAbstraction'
+import { zodResolver } from '@hookform/resolvers/zod'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import z from 'zod'
 
 function Page() {
+  const schema = z.object({
+    title: z
+      .string()
+      .min(2, "Name must be at least 2 characters")
+      .meta({
+        type: "text",
+        placeholder: "Enter title",
+      } satisfies FieldMeta.Text),
+    titleAr: z
+      .string()
+      .min(2, "Name must be at least 2 characters")
+      .meta({
+        type: "text",
+        placeholder: "Enter title",
+        label: "Title - Arabic",
+      } satisfies FieldMeta.Text),
+  });
+
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+    }
+  })
+
+  const onSubmit = async (values: z.infer<typeof schema>) => {
+    const id = toast.loading("Creating...");
+    console.log(values)
+    setTimeout(() => {
+      console.log("sent")
+    }, 3000);
+    toast.dismiss(id);
+    toast.success("Item created successfully");
+    form.reset()
+  }
   return (
-    <>
-      <HeroSection
-        title="Contact us"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quidem, sit rem mollitia qui quos corporis odit numquam nam earum sapiente rerum obcaecati"
-        mood="shared"
-      />
-    </>
+    <FormAbstraction
+      zodSchema={schema}
+      reactHookForm={form}
+      onSubmit={onSubmit}
+    />
   )
 }
 
