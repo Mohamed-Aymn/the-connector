d3b7c9f5a6771a534bc9355bd17be1d904465ed3
Mohamed Aymn <mohamedaymn218@gmail.com>
2025-09-04 17:46:13 +0300

    feat: clients section

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 import AboutSection from "@/components/pages/home/aboutSection";
+import ClientsSection from "@/components/pages/home/clientsSection";
 import HeroSection from "@/components/pages/home/heroSection";
 import ServicesSection from "@/components/pages/home/servicesSection";
 
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <ServicesSection />
+      <ClientsSection />
     </>
   );
 }
