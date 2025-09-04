02c0ed608e19743bd49073d59befe352006316ca
Mohamed Aymn <mohamedaymn218@gmail.com>
2025-09-04 16:18:46 +0300

    refactor: updated simple hero section

--- components/pages/home/hero.tsx
+++ components/pages/home/hero.tsx
@@ -1,38 +1,47 @@
-import LetterGlitch from '@/components/LetterGlitch'
+import Description from '@/components/shared/typography/description'
 import Heading from '@/components/shared/typography/heading'
-import { COLORS } from '@/lib/colors'
+import { Button } from '@/components/ui/button'
 import { useTranslations } from 'next-intl'
 import React from 'react'
 
 function Hero() {
   const t = useTranslations("Home.hero")
+  const contentClassName = "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center"
   return (
-    <div className="relative w-full h-screen">
-      {/* Fullscreen LetterGlitch */}
-      <div className="absolute inset-0 z-10">
-        <Heading size="md" className="text-secondary z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          {t("title")}
-        </Heading>
-        <LetterGlitch
-          glitchSpeed={70}
-          centerVignette={true}
-          outerVignette={false}
-          smooth={true}
-          glitchColors={[COLORS.PRIMARY, COLORS.DARK_NEUTRAL]}
-        />
-      </div>
-
-      {/* Clip container only showing the left polygon part */}
-      <div
-        className={`absolute z-20 bg-secondary inset-0 [clip-path:polygon(55%_0,100%_0,100%_100%,40%_100%)] overflow-hidden`}
-      >
-        <div className="absolute inset-0">
-          <Heading
-            size="md"
-            className="text-primary absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-          >
+    <div className="relative h-screen w-full">
+      {/* left half */}
+      <div className="w-full h-full bg-primary overflow-hidden">
+        <div className={`${contentClassName} text-secondary`}>
+          <Heading size="lg" >
             {t("title")}
           </Heading>
+          <Description size={'md'}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quidem, sit rem mollitia qui quos corporis odit numquam nam earum sapiente rerum obcaecati
+          </Description>
+          <Button className='mt-4' variant={'outline'} size={"lg"}>
+            {t("button")}
+          </Button>
+        </div>
+      </div>
+
+      {/* right half */}
+      <div
+        className={`absolute z-20 bg-secondary inset-0 [clip-path:polygon(50%_0,100%_0,100%_100%,50%_100%)] overflow-hidden`}
+      >
+        <div className="absolute inset-0">
+          <div className={`${contentClassName} text-primary`}>
+            <Heading
+              size="lg"
+            >
+              {t("title")}
+            </Heading>
+            <Description size={'md'}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quidem, sit rem mollitia qui quos corporis odit numquam nam earum sapiente rerum obcaecati
+            </Description>
+            <Button className='mt-4' size={"lg"}>
+              {t("button")}
+            </Button>
+          </div>
         </div>
       </div>
     </div>
