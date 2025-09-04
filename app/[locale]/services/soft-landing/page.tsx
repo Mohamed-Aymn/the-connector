c3c5ab9ee7af31ffab1601c0c6107683f639ead9
Mohamed Aymn <mohamedaymn218@gmail.com>
2025-09-04 23:19:26 +0300

    feat: soft landing page

--- app/[locale]/services/soft-landing/page.tsx
+++ app/[locale]/services/soft-landing/page.tsx
@@ -1,14 +1,26 @@
 import HeroSection from '@/components/shared/sections/heroSection'
+import Description from '@/components/shared/typography/description'
 import React from 'react'
 
 function Page() {
   return (
     <>
       <HeroSection
-        title="Soft Landing"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quidem, sit rem mollitia qui quos corporis odit numquam nam earum sapiente rerum obcaecati"
+        title="Soft Landing Across (MENA)"
+        description="This service provides comprehensive support for international businesses seeking to establish and expand their presence across Middle Eastern and North African markets."
         mood="service2"
       />
+      <Description size={'lg'}>
+        This specialized service offers a strategic entry point by navigating the complex regulatory
+        landscapes, diverse cultural nuances, and unique business ecosystems of countries
+        Especially the GCC and North Africa Region.
+
+        Service providers assist with essential elements including market entry strategy, legal
+        entity formation, local partnership development, officespace procurement, and regulatory
+        compliance across different MENA jurisdictions.
+
+        Connecting you with MENA business hubs and stakeholders.
+      </Description>
     </>
   )
 }
