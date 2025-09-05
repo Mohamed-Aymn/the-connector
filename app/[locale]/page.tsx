bba3d31a3069ffa9749a9919d2cb9c7d0fd57daa
Mohamed Aymn <mohamedaymn218@gmail.com>
2025-09-05 17:07:33 +0300

    feat: cta section in home page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,5 +1,6 @@
 import AboutSection from "@/components/pages/home/aboutSection";
 import ClientsSection from "@/components/pages/home/clientsSection";
+import CtaSection from "@/components/pages/home/ctaSection";
 import HeroSection from "@/components/pages/home/heroSection";
 import ServicesSection from "@/components/pages/home/servicesSection";
 import ValuesSection from "@/components/pages/home/valuesSection";
@@ -12,6 +13,7 @@ export default function Home() {
       <ServicesSection />
       <ValuesSection />
       <ClientsSection />
+      <CtaSection />
     </>
   );
 }
---
+++ components/pages/home/ctaSection.tsx
@@ -0,0 +1,16 @@
+import Section from '@/components/shared/layout/section'
+import Heading from '@/components/shared/typography/heading'
+import React from 'react'
+
+function CtaSection() {
+  return (
+    <Section type='outer'>
+      <Heading level={2} size='md'>
+        Contact Us
+      </Heading>
+      Should be a creative button
+    </Section>
+  )
+}
+
+export default CtaSection
