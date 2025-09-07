12f179cdaab161d852c24e0c2df697bace203667
Mohamed Aymn <mohamedaymn218@gmail.com>
2025-09-07 11:32:21 +0300

    fix: our services cards styles

--- components/pages/home/servicesSection.tsx
+++ components/pages/home/servicesSection.tsx
@@ -7,6 +7,57 @@ import { Link } from '@/i18n/navigation'
 import { useTranslations } from 'next-intl'
 import React from 'react'
 
+interface ContentProps {
+  type: 1 | 2;
+  title: string;
+  href: string;
+  description: string
+}
+
+function Content({ type, title, href, description }: ContentProps) {
+  return (
+
+    <Card
+      className={`
+              w-full  transition-colors duration-300
+              bg-transparent relative
+              border-0
+            ${type === 1 ?
+          "text-secondary" :
+          "text-primary"
+        }`}
+
+    // className={`
+    //         w-full  transition-colors duration-300
+    //         bg-transparent relative
+    >
+      {/* <div className={`
+            ${type === 2 ? "bg-secondary" : "bg-primary"}
+              absolute top-0 left-0 w-2/5 h-2/5 z-0`} /> */}
+
+      <div className='z-1'>
+        <CardHeader className="flex items-center justify-between">
+          <Heading level={3} size="sm" className="text-left">
+            {title}
+          </Heading>
+          <CardAction>
+            <Button className='cursor-pointer'>
+              <Link href={href as string}>
+                View More
+              </Link>
+            </Button>
+          </CardAction>
+        </CardHeader>
+        <CardContent className="leading-relaxed">
+          <Description size={'md'}>
+            {description}
+          </Description>
+        </CardContent>
+      </div>
+    </Card>
+  )
+}
+
 function ServicesSection() {
   const t = useTranslations("Home.OurServicesSection")
   const services = t.raw("items") as { title: string; description: string, type?: 1 | 2, href?: string }[]
@@ -27,37 +78,41 @@ function ServicesSection() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         {services.map((service, idx) => (
-          <Card key={idx}
-            className={`
-              w-full  transition-colors duration-300
-              ${service.type === 1 ?
-                "hover:bg-primary hover:text-secondary" :
-                "hover:bg-secondary hover:text-primary"
-              }
-            `}
-          >
-            <CardHeader className="flex items-center justify-between">
-              <Heading level={3} size="sm" className="text-left">
-                {service.title}
-              </Heading>
-              <CardAction>
-                <Button className='cursor-pointer'>
-                  <Link href={service.href as string}>
-                    View More
-                  </Link>
-                </Button>
-              </CardAction>
-            </CardHeader>
-            <CardContent className="leading-relaxed">
-              <Description size={'md'} className={`
-              ${service.type === 1 ? "hover:text-secondary" : ""}
-              `}>
-                {service.description}
-              </Description>
-            </CardContent>
-          </Card>
-        ))}
-      </div>
+          <div key={idx} className="relative w-full h-full group">
+            <div className={`w-full h-full overflow-hidden 
+            ${service.type === 1 ? "bg-primary" : "bg-secondary"}
+              rounded-lg`}>
+              <div>
+                <Content
+                  type={service.type as 1 | 2}
+                  title={service.title}
+                  href={service.href as string}
+                  description={service.description}
+                />
+              </div>
+            </div>
+
+            <div
+              className={`
+        absolute z-20 inset-0
+        bg-white
+        [clip-path:polygon(30%_0,100%_0,100%_100%,30%_100%)]
+        group-hover:[clip-path:polygon(50%_0,100%_0,100%_100%,50%_100%)]
+        transition-all duration-500 ease-in-out
+        overflow-hidden
+      `}
+            >
+              <div>
+                <Content
+                  type={service.type as 1 | 2}
+                  title={service.title}
+                  href={service.href as string}
+                  description={service.description}
+                />
+              </div>
+            </div>
+          </div>
+        ))}     </div>
     </Section>
   )
 }
